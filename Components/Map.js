42c97ab5a75566e15d92e3faf0ff87d08bd70cc5
MariusRobin <marius.rob1@gmail.com>
2019-07-01 09:39:22 +0200

    error detail

--- Components/Map.js
+++ Components/Map.js
@@ -6,13 +6,31 @@ class Map extends React.Component{
     render(){
 
         return(
-            <Text>map yesyesyo</Text>
+            <View style={styles.container}>
+                <Text> YOYOYO </Text>
+            </View>
 
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header:{
+        flexDirection:"row"
+    },
+    picker: {
+        width: 200,
+        backgroundColor: '#FFF0E0',
+        borderColor: 'black',
+        borderWidth: 1,
+    },
+    icon:{
+        height:40,
+        width:40
+    }
+});
 
-})
 export default Map
