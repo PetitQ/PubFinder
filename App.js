8063d7228e1c3aa23d5c8c06d73d9d0951bf6e36
quentin petit <quentin.petit@u-bordeaux.fr>
2019-10-07 14:39:44 +0200

    PubFinder

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Navigation from './Navigation/Navigation';
 import { Provider } from 'react-redux'
 import Store from './Store/configureStore'
+
+
 export default class App extends React.Component {
   render() {
     return (
